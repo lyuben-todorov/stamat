f3ed2384eeac198faff086df96bf1f891ee9cf67
Lyuben Todorov <lyuben.todorov@gmail.com>
2020-09-10 22:40:44 +0300

    Add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,14 +10,15 @@ import userRouter from './routes/auth';
 import statsticsRouter from './routes/statsistics'
 import _ from 'lodash';
 
-
 const app = express();
-
-
-
 
 mongoClient.startSession();
 //middleware
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
